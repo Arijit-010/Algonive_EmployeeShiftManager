e01acaf617b213147619f99abb7305f1339dcdb0
Arijit Ghosh <ghosharijit149@gmail.com>
2026-02-05 23:55:37 +0530

    first commit

---
+++ Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    int id;
+    String name;
+    String role;
+
+    public Employee(int id, String name, String role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+}
+
